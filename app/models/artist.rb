b048c9d076a7d96d0e4fb255d7503951dbd686fb
dangnhon <dang.nhon12345@yahoo.com>
2020-12-15 18:06:00 -0800

    wowza

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        count_song = Song.all
+        count_song.length
+      end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+  def artist_name
+    song_artist = Song.all
+    song_artist.select do |song| 
+      if song.artist.id == self.artist.id
+        return song.artist.name 
+      end 
+    end 
+  end 
+
 end
